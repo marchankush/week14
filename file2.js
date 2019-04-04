54978699d1562afd99d815dd33e197568c0e85fc
Ankush Ankush <743847@student.tor.loc>
2019-04-04 11:17:35 -0400

    bolotararara

---
+++ file2.js
@@ -0,0 +1,10 @@
+var user = { 
+    first_Name: "CSS",
+    last_Name: "HTML",
+    age: 4,/*from w w w.java 2 s  . o m*/
+    website: "java2s.com"
+};
+
+for (key in user){
+    console.log(key);
+}
